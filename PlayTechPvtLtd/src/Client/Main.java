759b2e461d24f763ead7bc0b86081472e8877ff7
Cha1552164 <chamithgithub>
2022-08-15 22:29:56 +0530

    update stylies modification

--- PlayTechPvtLtd/src/Client/Main.java
+++ PlayTechPvtLtd/src/Client/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../view/loginForm.fxml"));
-        primaryStage.setTitle("Messenger!");
+        primaryStage.setTitle("live Chat!");
         primaryStage.setScene(new Scene(root, 382, 560));
         primaryStage.setResizable(false);
         primaryStage.show();
